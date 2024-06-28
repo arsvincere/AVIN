5dcf4b0a1076a965e053fbee2d40954975a580ce
Alex Avin <mr.alexavin@gmail.com>
2024-06-28 16:29:40 +0300

    add simple signal/slot realization

--- avin/utils.py
+++ avin/utils.py
@@ -366,6 +366,24 @@ class Cmd():# {{{
             os.makedirs(dir_path)
 # }}}
 # }}}
+class Signal():# {{{
+    def __init__(self, *args):# {{{
+        self.args = args
+        self.__slots = list()
+    # }}}
+    def __checkTypes(self, args: tuple):# {{{
+        for i, j in zip(args, self.args):
+            assert type(i) == j
+    # }}}
+    def emit(self, *args):# {{{
+        for receiver in self.__slots:
+            self.__checkTypes(args)
+            receiver(*args)
+    # }}}
+    def connect(self, slot_func):# {{{
+        self.__slots.append(slot_func)
+    # }}}
+# }}}
 
 def now():# {{{
     return datetime.now(timezone.utc)
