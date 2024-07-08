8c67e60a96d4347d8f2dbb963a9c09aa15ae3075
Alex Avin <mr.alexavin@gmail.com>
2024-07-08 15:15:29 +0300

    module avin - import all classes

---
+++ avin/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env  python3
+# ============================================================================
+# URL:          http://arsvincere.com
+# AUTHOR:       Alex Avin
+# E-MAIL:       mr.alexavin@gmail.com
+# LICENSE:      GNU GPLv3
+# ============================================================================
+
+from avin.const import *
+from avin.data import *
+from avin.core import *
+from avin.logger import *
+from avin.utils import *
+
