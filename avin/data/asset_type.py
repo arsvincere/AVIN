d889cb48cb142f22583d9ee1a63b4801160cef6e
Alex Avin <mr.alexavin@gmail.com>
2024-08-04 21:07:13 +0300

    fmt

--- avin/data/asset_type.py
+++ avin/data/asset_type.py
@@ -8,29 +8,32 @@
 
 import enum
 
-class AssetType(enum.Enum):# {{{
-    UNDEFINE    = 0
-    CASH        = 1
-    INDEX       = 2
-    SHARE       = 3
-    BOND        = 4
-    FUTURE      = 5
-    OPTION      = 6
-    CURRENCY    = 7
-    ETF         = 8
 
+class AssetType(enum.Enum):  # {{{
+    UNDEFINE = 0
+    CASH = 1
+    INDEX = 2
+    SHARE = 3
+    BOND = 4
+    FUTURE = 5
+    OPTION = 6
+    CURRENCY = 7
+    ETF = 8
 
-    @classmethod  #fromStr# {{{
+    @classmethod  # fromStr# {{{
     def fromStr(cls, string):
         types = {
-            "CASH":     AssetType.CASH,
-            "INDEX":    AssetType.INDEX,
-            "SHARE":    AssetType.SHARE,
-            "BOND":     AssetType.BOND,
-            "FUTURE":   AssetType.FUTURE,
+            "CASH": AssetType.CASH,
+            "INDEX": AssetType.INDEX,
+            "SHARE": AssetType.SHARE,
+            "BOND": AssetType.BOND,
+            "FUTURE": AssetType.FUTURE,
             "CURRENCY": AssetType.CURRENCY,
-            "ETF":      AssetType.ETF,
-            }
+            "ETF": AssetType.ETF,
+        }
         return types[string]
+
     # }}}
+
+
 # }}}
