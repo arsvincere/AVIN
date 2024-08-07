e8fbbea07ca2cc3e3baaa2c54d62a9d602dff05b
Alex Avin <mr.alexavin@gmail.com>
2024-08-07 20:52:59 +0300

    fmt

--- avin/data/source.py
+++ avin/data/source.py
@@ -7,28 +7,34 @@
 # ============================================================================
 
 from __future__ import annotations
+
 import enum
+
 from avin.utils import Cmd
 
-class Source(enum.Enum):# {{{
-    UNDEFINE    = 0
-    MOEX        = 1
-    TINKOFF     = 2
 
-    @classmethod  #save# {{{
+class Source(enum.Enum):  # {{{
+    UNDEFINE = 0
+    MOEX = 1
+    TINKOFF = 2
+
+    @classmethod  # save# {{{
     def save(cls, source: Source, file_path: str):
         string = source.name
         Cmd.write(string, file_path)
+
     # }}}
-    @classmethod  #load# {{{
+    @classmethod  # load# {{{
     def load(cls, file_path):
         string = Cmd.read(file_path).strip()
         sources = {
             "UNDEFINE": Source.UNDEFINE,
-            "MOEX":     Source.MOEX,
-            "TINKOFF":  Source.TINKOFF,
-            }
+            "MOEX": Source.MOEX,
+            "TINKOFF": Source.TINKOFF,
+        }
         return sources[string]
-    # }}}
-# }}}
 
+    # }}}
+
+
+# }}}
