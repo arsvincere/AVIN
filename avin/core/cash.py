06c4c73bbd381ca6fe0db46011873af25b7e94f9
Alex Avin <mr.alexavin@gmail.com>
2024-08-07 20:52:50 +0300

    fmt

--- avin/core/cash.py
+++ avin/core/cash.py
@@ -7,36 +7,44 @@
 # ============================================================================
 
 from __future__ import annotations
-import enum
+
 import abc
+import enum
 from datetime import datetime
+
 from avin.const import Usr
-from avin.data import Id, Exchange, AssetType, Data
 from avin.core.chart import Chart
 from avin.core.timeframe import TimeFrame
-from avin.utils import Cmd, now
+from avin.data import AssetType, Data, Exchange, InstrumentId
 from avin.logger import logger
+from avin.utils import Cmd, now
 
 
-class Cash():# {{{
-    class Type(enum.Enum):# {{{
+class Cash:  # {{{
+    class Type(enum.Enum):  # {{{
         UNDEFINE = 0
         RUB = 1
+
     # }}}
-    def __init__(self, cash_type: Cash.Type, value: float):# {{{
+    def __init__(self, cash_type: Cash.Type, value: float):  # {{{
         self.__type = cash_type
         self.__value = value
+
     # }}}
     @property  # type#{{{
     def type(self) -> Cash.Type:
         return self.__type
+
     # }}}
     @property  # value#{{{
     def value(self) -> float:
         return self.__value
+
     @value.setter
     def value(self, value: float):
         self.__value = value
-    # }}}
-# }}}
 
+    # }}}
+
+
+# }}}
