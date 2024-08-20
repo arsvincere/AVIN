45f5cfbff1dcc887dcae0851a75477d3124f16c4
Alex Avin <mr.alexavin@gmail.com>
2024-08-20 11:56:03 +0300

    remove old methods save/load, add fromRecord

--- avin/data/data_source.py
+++ avin/data/data_source.py
@@ -11,39 +11,25 @@ from __future__ import annotations
 import abc
 import enum
 
-from avin.utils import Cmd
-
 
 class DataSource(enum.Enum):  # {{{
     MOEX = 1
     TINKOFF = 2
 
+    @classmethod  # fromStr  # {{{
+    def fromStr(cls, string):
+        sources = {
+            "MOEX": DataSource.MOEX,
+            "TINKOFF": DataSource.TINKOFF,
+        }
+        return sources[string]
+
+    # }}}
     @classmethod  # fromRecord  # {{{
     def fromRecord(cls, record):
         string = record["source"]
-        sources = {
-            "MOEX": DataSource.MOEX,
-            "TINKOFF": DataSource.TINKOFF,
-        }
-        return sources[string]
-
-    # }}}
-    @classmethod  # save# {{{
-    def save(cls, source: DataSource, file_path: str):
-        # DEPICATE
-        string = source.name
-        Cmd.write(string, file_path)
-
-    # }}}
-    @classmethod  # load# {{{
-    def load(cls, file_path):
-        # DEPICATE
-        string = Cmd.read(file_path).strip()
-        sources = {
-            "MOEX": DataSource.MOEX,
-            "TINKOFF": DataSource.TINKOFF,
-        }
-        return sources[string]
+        source = cls.fromStr(string)
+        return source
 
     # }}}
 
