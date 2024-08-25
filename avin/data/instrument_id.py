0539ed7a28b3b8f18b23cd487f6283af8fb52b66
Alex Avin <mr.alexavin@gmail.com>
2024-08-25 15:08:43 +0300

    InstrumentId add checkArgs

--- avin/data/instrument_id.py
+++ avin/data/instrument_id.py
@@ -88,6 +88,9 @@ class InstrumentId:  # {{{
     # }}}
     @classmethod  # byFigi# {{{
     async def byFigi(cls, figi: str) -> InstrumentId:
+        if not cls.__checkArgs(figi=figi):
+            assert False
+
         id_list = await Keeper.get(InstrumentId, figi=figi)
 
         assert len(id_list) == 1
@@ -101,6 +104,11 @@ class InstrumentId:  # {{{
     ) -> InstrumentId:
         logger.debug(f"{cls.__name__}.byTicker()")
 
+        if not cls.__checkArgs(
+            asset_type=asset_type, exchange=exchange, ticker=ticker
+        ):
+            assert False
+
         id_list = await Keeper.get(
             InstrumentId,
             asset_type=asset_type,
@@ -111,6 +119,29 @@ class InstrumentId:  # {{{
         assert len(id_list) == 1
         ID = id_list[0]
         return ID
+
+    # }}}
+    @classmethod  # __checkArgs{{{
+    def __checkArgs(
+        cls,
+        asset_type=None,
+        exchange=None,
+        ticker=None,
+        figi=None,
+        name=None,
+    ):
+        if asset_type:
+            assert isinstance(asset_type, AssetType)
+        if exchange:
+            assert hasattr(exchange, "name")
+        if ticker:
+            assert isinstance(ticker, str)
+        if figi:
+            assert isinstance(figi, str)
+        if name:
+            assert isinstance(name, str)
+
+        return True
 
     # }}}
 
