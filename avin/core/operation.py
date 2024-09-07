62cbc3f35f7974e981963ebac6b8114b6b64288b
Alex Avin <mr.alexavin@gmail.com>
2024-09-07 10:38:11 +0300

    type hints

--- avin/core/operation.py
+++ avin/core/operation.py
@@ -10,28 +10,12 @@ from __future__ import annotations
 
 import enum
 from datetime import datetime
+from typing import Optional
 
 from avin.config import Usr
 from avin.core.id import Id
 from avin.data import InstrumentId
 from avin.keeper import Keeper
-
-# TODO: ID = сделай явными их установку а то пиздец потом...
-# когда брокер получает операцию по ордеру - присваивается
-# это операции каждый раз новое ИД...
-# надо еще раз подумать и решить кто где когда будет присваивать
-# операциям ИД, точно не брокер.
-# не факт что аккаунт.
-# возможно вообще стратегия, и стратегия же будет запрашивать операцию
-# по своему трейду!!!!!!! Получится так?
-# ну смотри ордер filled - этот сигнал же может и стратегия ловить и трейд
-# трейд не может запросить свою операцию, он тупой должен быть.
-# а вот стратегия вполне...
-# может вообще все это гавно от трейда отвязать с подключением ордеров и
-# операций, может пусть этим базовый класс стратегии занимается?
-# если так получится было бы хорошо.
-# Нет, не получится. Ордер привязан к трейду. Если ордер привязать
-# к стратегии то придется разбираться к какому трейду это добро приехало.
 
 
 class Operation:  # {{{
@@ -64,10 +48,10 @@ class Operation:  # {{{
         price: float,
         amount: float,
         commission: float,
-        operation_id: Id = None,
-        order_id: Id = None,  # XXX: а что если это обязательный аргумент?
-        trade_id: Id = None,
-        meta: object = None,
+        operation_id: Optional[Id] = None,
+        order_id: Optional[Id] = None,
+        trade_id: Optional[Id] = None,
+        meta: Optional[str] = None,
     ):
         self.account_name = account_name
         self.dt = dt
@@ -78,7 +62,7 @@ class Operation:  # {{{
         self.quantity = quantity
         self.amount = amount
         self.commission = commission
-        self.operation_id = operation_id if operation_id else Id.newId(self)
+        self.operation_id = operation_id
         self.order_id = order_id
         self.trade_id = trade_id
         self.meta = meta
@@ -96,20 +80,12 @@ class Operation:  # {{{
 
     # }}}
     async def setParentTrade(self, trade):  # {{{
-        # FIX: тиньков при создании операции уже присваивает трейд_ид
-        # копирует его из ордера, так что этот метод и еще одно
-        # обновление в БД бессмысленны, подумай как бы это все сделать
-        # более очевидным и прозрачным...
-        # может у тинькова убрать заполнение этого поля?
-        # а смысл?
-        # а зачем тогда аттачить операцию к трейду?
-        # а стоп
         self.trade_id = trade.trade_id
         await Operation.update(self)
 
     # }}}
     @classmethod  # fromRecord{{{
-    async def fromRecord(cls, record):
+    async def fromRecord(cls, record: asyncpg.Record) -> Operation:
         ID = await InstrumentId.byFigi(record["figi"])
 
         op = Operation(
@@ -131,25 +107,23 @@ class Operation:  # {{{
 
     # }}}
     @classmethod  # save{{{
-    async def save(cls, operation):
-        # XXX: а может тут перезапись в кипере делать?
-        # сейчас будет исключение если операция уже добавлена в бд
+    async def save(cls, operation: Operation) -> None:
         await Keeper.add(operation)
 
     # }}}
     @classmethod  # load{{{
-    async def load(cls, operation_id):
+    async def load(cls, operation_id: Id) -> Operation:
         op = await Keeper.get(cls, operation_id=operation_id)
         return op
 
     # }}}
     @classmethod  # delete{{{
-    async def delete(cls, operation):
+    async def delete(cls, operation: Operation) -> None:
         await Keeper.delete(operation)
 
     # }}}
     @classmethod  # update{{{
-    async def update(cls, operation):
+    async def update(cls, operation: Operation) -> None:
         await Keeper.update(operation)
 
     # }}}
