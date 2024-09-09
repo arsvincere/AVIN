c8e04c6b4143de839eee21a46c11303f1806fa42
Alex Avin <mr.alexavin@gmail.com>
2024-09-09 15:27:20 +0300

    add module exceptions.py

---
+++ avin/exceptions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env  python3
+# ============================================================================
+# URL:          http://arsvincere.com
+# AUTHOR:       Alex Avin
+# E-MAIL:       mr.alexavin@gmail.com
+# LICENSE:      GNU GPLv3
+# ============================================================================
+
+__all__ = (
+    "AssetError",
+    "BrokerError",
+)
+
+
+class AssetError(Exception): ...
+
+
+class BrokerError(Exception): ...
