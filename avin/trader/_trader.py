326e692e9341807449602556891360313a045f5b
Alex Avin <mr.alexavin@gmail.com>
2024-09-26 10:24:08 +0300

    upd trader

--- avin/trader/_trader.py
+++ avin/trader/_trader.py
@@ -11,19 +11,7 @@ import asyncio
 from avin.core import AssetList, Event, NewBarEvent, Strategy, TimeFrame
 from avin.data import Data
 from avin.trader.tinkoff import Tinkoff
-from avin.utils import logger
-
-# FIX: обновление данных сейчас идет с моэкс а там задержка...
-# после обновления данных в БД и загрузки графиков - надо еще
-# раз обновлять данные но уже у брокера запрашивать
-
-# TODO: вот примерно так выглядеть будет попытка создания
-# дата стрима и пересоздание если не получилось
-# сделай из этого говнокода цикл аккуратный с нормальными логами
-# и запихай в какой нибудь метод типо
-# trader.__createDataStream
-
-# TODO: причесать логи и отступы в них
+from avin.utils import logger, now
 
 
 class Trader:
@@ -33,7 +21,7 @@ class Trader:
         self.event = None
 
     # }}}
-    async def __loadConfig(self):  # {{{
+    async def __loadConfig(self) -> None:  # {{{
         logger.info(":: Trader load config")
         self.cfg = {
             "broker": Tinkoff,
@@ -47,12 +35,12 @@ class Trader:
         }
 
     # }}}
-    async def __loadTimeTable(self):  # {{{
+    async def __loadTimeTable(self) -> None:  # {{{
         logger.info(":: Trader load timetable")
         self.timetable = None
 
     # }}}
-    async def __loadStrategyes(self):  # {{{
+    async def __loadStrategyes(self) -> None:  # {{{
         logger.info(":: Trader load strategyes")
         self.strategyes = list()
         for i in self.cfg["strategy_list"]:
@@ -60,7 +48,7 @@ class Trader:
             self.strategyes.append(strategy)
 
     # }}}
-    async def __loadTimeFrameList(self):  # {{{
+    async def __loadTimeFrameList(self) -> None:  # {{{
         logger.info(":: Trader load timeframe list")
 
         self.timeframe_list = list()
@@ -69,25 +57,29 @@ class Trader:
             self.timeframe_list.append(timeframe)
 
     # }}}
-    async def __loadBroker(self):  # {{{
+    async def __loadBroker(self) -> None:  # {{{
         logger.info(":: Trader load broker")
         self.broker = self.cfg["broker"]
 
     # }}}
-    async def __makeGeneralAssetList(self):  # {{{
+    async def __makeGeneralAssetList(self) -> None:  # {{{
         logger.info(":: Trader make asset list")
         self.alist = await AssetList.load("Trio")
 
     # }}}
-    async def __cacheAssetsInfo(self):  # {{{
+    async def __cacheAssetsInfo(self) -> None:  # {{{
         logger.info(":: Trader caching assets info")
+        assert self.alist is not None
+
         for asset in self.alist:
             logger.info(f"  - caching {asset}")
             await asset.cacheInfo()
 
     # }}}
-    async def __updateHistoricalData(self):  # {{{
+    async def __updateHistoricalData(self) -> None:  # {{{
         logger.info(":: Trader update historical data")
+        assert self.alist is not None
+
         for asset in self.alist:
             for timeframe in self.timeframe_list:
                 await Data.update(
@@ -96,13 +88,13 @@ class Trader:
                 )
 
     # }}}
-    async def __updateAnalytic(self):  # {{{
+    async def __updateAnalytic(self) -> None:  # {{{
         logger.info(":: Trader update data analytic")
         # for asset in self.alist:
         #     self.analytic.updateAll(asset)
 
     # }}}
-    async def __setAccount(self):  # {{{
+    async def __setAccount(self) -> None:  # {{{
         logger.info(":: Trader set account")
 
         account = await self.broker.getAccount(self.cfg["account"])
@@ -114,42 +106,61 @@ class Trader:
             assert False, "Account not found"
 
     # }}}
-    async def __cacheCharts(self):  # {{{
+    async def __cacheCharts(self) -> None:  # {{{
         logger.info(":: Trader caching all chart")
+        assert self.alist is not None
+
         timeframe_list = self.cfg["timeframe_list"]
         for asset in self.alist:
             for timeframe in timeframe_list:
-                logger.info(f"  - caching chart {asset.ticker}-{timeframe}")
+                logger.info(f"  caching chart {asset.ticker}-{timeframe}")
                 await asset.cacheChart(timeframe)
 
     # }}}
-    async def __updateRealTimeData(self):  # {{{
+    async def __updateRealTimeData(self) -> None:  # {{{
         logger.info(":: Trader update real time data")
 
-        # TODO: тут добавлять бары к графикам в озу
-        # пока у них сигналы не подключены
+        for asset in self.alist:
+            for timeframe in self.timeframe_list:
+                chart = asset.chart(timeframe)
+                begin = chart.last.dt + timeframe
+                end = now()
+                new_bars = await self.broker.getHistoricalBars(
+                    asset, timeframe, begin, end
+                )
+                count = len(new_bars)
+                if count:
+                    logger.info(
+                        f"  {asset.ticker}-{timeframe} add {count} bars"
+                    )
+                    for bar in new_bars:
+                        chart.addNewBar(bar)
 
     # }}}
-    async def __startStrategyes(self):  # {{{
+    async def __startStrategyes(self) -> None:  # {{{
         logger.info(":: Trader start strategyes")
         for strategy in self.strategyes:
             await strategy.start()
 
     # }}}
-    async def __connectStrategyes(self):  # {{{
+    async def __connectStrategyes(self) -> None:  # {{{
         logger.info(":: Trader connect strategyes")
+        assert self.alist is not None
+
         for strategy in self.strategyes:
             await strategy.connect(self.alist)
 
     # }}}
-    async def __createTransactionStream(self):  # {{{
+    async def __createTransactionStream(self) -> None:  # {{{
         logger.info(":: Trader create transaction stream")
 
         await self.broker.createTransactionStream(self.account)
 
     # }}}
-    async def __createBarStream(self):  # {{{
+    async def __createBarStream(self) -> None:  # {{{
         logger.info(":: Trader make data stream")
+        assert self.alist is not None
+
         for asset in self.alist:
             for timeframe in self.timeframe_list:
                 await self.broker.createBarStream(asset, timeframe)
@@ -157,13 +168,13 @@ class Trader:
         await self.broker.new_bar.async_connect(self.__onNewBar)
 
     # }}}
-    async def __startTransactionStream(self):  # {{{
+    async def __startTransactionStream(self) -> None:  # {{{
         logger.info(":: Trader start transaction stream")
 
         await self.broker.startTransactionStream()
 
     # }}}
-    async def __startBarStream(self):  # {{{
+    async def __startBarStream(self) -> None:  # {{{
         logger.info(":: Trader start data stream")
         result = await self.broker.startDataStream()
 
@@ -176,55 +187,57 @@ class Trader:
             result = await self.broker.startDataStream()
 
     # }}}
-    async def __mainCycle(self):  # {{{
+    async def __mainCycle(self) -> None:  # {{{
         logger.info(":: Trader run main cycle")
         self.work = True
         while self.work:
-            # await self.__ensureConnection()
-            # await self.__ensureMarketOpen()
+            await self.__ensureConnection()
+            await self.__ensureMarketOpen()
             await asyncio.sleep(60)
 
         await self.__finishWork()
 
     # }}}
-    async def __ensureConnection(self):  # {{{
+    async def __ensureConnection(self) -> None:  # {{{
         logger.info(":: Trader ensure connection")
         result = await self.broker.connect()
 
         while not result:
-            logger.info("  sleep 1 minute...")
-            await asyncio.sleep(60)
+            logger.info("  sleep 20 seconds")
+            await asyncio.sleep(20)
             logger.info("  try again")
             result = await self.broker.connect()
 
         logger.info("  connection ok")
 
     # }}}
-    async def __ensureMarketOpen(self):  # {{{
+    async def __ensureMarketOpen(self) -> None:  # {{{
         """
         Loop until the market is openly.
-        :return: when market is available for trading
+        Return when market is available for trading
         """
         logger.info(":: Trader ensure market open")
 
         status = await self.broker.isMarketOpen()
         while not status:
-            logger.info("  waiting to open market, sleep 1 minute")
-            await asyncio.sleep(60)
-            status = self.broker.isMarketOpen()
+            logger.info("  waiting to open market, sleep 20 seconds")
+            await asyncio.sleep(20)
+            status = await self.broker.isMarketOpen()
 
         logger.info("  market is open!")
 
     # }}}
-    async def __onNewBar(self, event: NewBarEvent):  # {{{
-        logger.info(f"-> receive event {event}")
+    async def __onNewBar(self, event: NewBarEvent) -> None:  # {{{
+        logger.info(f"-> receive bar {event}")
         assert event.type == Event.Type.NEW_BAR
+        assert self.alist is not None
 
         asset = self.alist.find(figi=event.figi)
+        assert asset is not None
         await asset.receiveNewBar(event)
 
     # }}}
-    async def __finishWork(self):  # {{{
+    async def __finishWork(self) -> None:  # {{{
         for strategy in self.strategyes:
             await strategy.finish()
         ...
@@ -241,7 +254,7 @@ class Trader:
         self.work = False
 
     # }}}
-    async def initialize(self):  # {{{
+    async def initialize(self) -> None:  # {{{
         logger.info(":: Trader start initialization")
         await self.__loadConfig()
         await self.__loadTimeTable()
@@ -254,7 +267,7 @@ class Trader:
         await self.__updateAnalytic()
 
     # }}}
-    async def run(self):  # {{{
+    async def run(self) -> None:  # {{{
         logger.info(":: Trader run")
         await self.__ensureConnection()
         await self.__setAccount()
@@ -269,7 +282,7 @@ class Trader:
         await self.__mainCycle()
 
     # }}}
-    async def stop(self):  # {{{
+    async def stop(self) -> None:  # {{{
         if self.work:
             logger.info(":: Trader shuting down")
             self.work = False
