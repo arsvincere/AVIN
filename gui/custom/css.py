8aaf4c1da31a3f4d2ecc8629639aaf746be38bff
Alex Avin <mr.alexavin@gmail.com>
2024-11-22 15:44:35 +0300

    add: css tab widget

--- gui/custom/css.py
+++ gui/custom/css.py
@@ -25,6 +25,52 @@ class Css:
         color: {Theme.fg_normal};
         """
     # }}}
+    # TAB_WIDGET  # {{{
+    # надо на QTabBar переходить
+    TAB_WIDGET = f"""
+        font-family: Monospace;
+        background-color: {Theme.bg_normal};
+        color: {Theme.fg_normal};
+
+        QTabWidget {{
+            font-family: Monospace;
+            background-color: {Theme.bg_normal};
+            color: {Theme.fg_normal};
+        }}
+        QTabWidget::pane {{
+            border-top: 2px solid #C2C7CB;
+        }}
+        QTabWidget::tab-bar {{
+            // left: 50px;
+        }}
+
+        QTabBar::tab {{
+            background-color: red;
+            border: 2px solid #C4C4C3;
+            border-bottom-color: #C2C7CB; /* same as the pane color */
+            border-top-left-radius: 4px;
+            border-top-right-radius: 4px;
+            min-width: 8ex;
+            padding: 2px;
+        }}
+
+        QTabBar::tab:selected, QTabBar::tab:hover {{
+            background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                        stop: 0 #fafafa, stop: 0.4 #f4f4f4,
+                                        stop: 0.5 #e7e7e7, stop: 1.0 #fafafa);
+        }}
+
+        QTabBar::tab:selected {{
+            border-color: #9B9B9B;
+            border-bottom-color: #C2C7CB; /* same as pane color */
+        }}
+
+        QTabBar::tab:!selected {{
+            margin-top: 2px; /* make non-selected tabs look smaller */
+        }}
+
+        """
+    # }}}
     # LINE_EDIT  # {{{
     LINE_EDIT = f"""
         QLineEdit {{
