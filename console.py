0b517638763212debfe75798ff765fd5663b0a3b
Alex Avin <mr.alexavin@gmail.com>
2024-12-02 14:24:55 +0300

    add: console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env  python3
+# ============================================================================
+# URL:          http://arsvincere.com
+# AUTHOR:       Alex Avin
+# E-MAIL:       mr.alexavin@gmail.com
+# LICENSE:      GNU GPLv3
+# ============================================================================
+
+
+def main():
+    # start app
+    print("Welcome to AVIN Trade System!")
+
+    # load trader config
+
+    # start trader
+
+    # before quit actions
+    print("Goodbuy!")
+
+
+if __name__ == "__main__":
+    main()
