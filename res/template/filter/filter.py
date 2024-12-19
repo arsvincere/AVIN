a8302cef181a98a78dac7be0ba2463dc34ef5d65
Alex Avin <mr.alexavin@gmail.com>
2024-12-19 16:29:45 +0300

    upd: template filter (chart, trade, asset)

--- res/template/filter/filter.py
+++ res/template/filter/filter.py
@@ -1,9 +1,29 @@
 import avin
 
 
-def condition(asset: avin.Asset) -> bool:
-    chart = asset.chart("D")
+def condition(item: avin.Asset | avin.Trade | avin.Chart) -> bool:
+    class_name = item.__class__.__name__
+    match class_name:
+        case "Asset":
+            return condition_asset(item)
+        case "Trade":
+            return condition_trade(item)
+        case "Chart":
+            return condition_chart(item)
 
+
+def condition_asset(asset: avin.Asset) -> bool:
+    assert False, "todo it"
+
+
+def condition_trade(trade: avin.Trade) -> bool:
+    assert False, "todo it"
+
+
+def condition_chart(chart: avin.Chart) -> bool:
+    assert False, "todo it"
+
+    # Example:
     # if chart[3] is None:
     #     return False
     #
@@ -13,5 +33,5 @@ def condition(asset: avin.Asset) -> bool:
     #
     # if b3.isBull() and b2.isBull() and b1.isBull():
     #     return True
-
-    return False
+    #
+    # return False
