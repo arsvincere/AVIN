19afa2eb49ac8d5d78d89c2bf94571519363ae6d
Alex Avin <mr.alexavin@gmail.com>
2025-02-04 18:04:41 +0300

    upd: Size / SimpleSize

--- avin/core/size.py
+++ avin/core/size.py
@@ -12,23 +12,40 @@ import enum
 
 
 class Size(enum.Enum):
-    BLACKSWAN_SMALL = -7
-    # GREATEST_SMALL = -6
-    ANOMAL_SMALL = -6
-    EXTRA_SMALL = -5
-    VERY_SMALL = -4
-    SMALLEST = -3
-    SMALLER = -2
-    SMALL = -1
-    NORMAL = 0
-    BIG = 1
-    BIGGER = 2
-    BIGGEST = 3
-    VERY_BIG = 4
-    EXTRA_BIG = 5
-    ANOMAL_BIG = 6
-    # GREATEST_BIG = -6
-    BLACKSWAN_BIG = 7
+    # BLACKSWAN_SMALL = -8
+    # GREATEST_SMALL = -7
+    # ANOMAL_SMALL = -6
+    # EXTRA_SMALL = -5
+    # VERY_SMALL = -4
+    # SMALLEST = -3
+    # SMALLER = -2
+    # SMALL = -1
+    # NORMAL = 0
+    # BIG = 1
+    # BIGGER = 2
+    # BIGGEST = 3
+    # VERY_BIG = 4
+    # EXTRA_BIG = 5
+    # ANOMAL_BIG = 6
+    # GREATEST_BIG = 7
+    # BLACKSWAN_BIG = 8
+    BLACKSWAN_SMALL = 0
+    GREATEST_SMALL = 1
+    ANOMAL_SMALL = 3
+    EXTRA_SMALL = 5
+    VERY_SMALL = 10
+    SMALLEST = 20
+    SMALLER = 30
+    SMALL = 40
+    NORMAL = 60
+    BIG = 70
+    BIGGER = 80
+    BIGGEST = 90
+    VERY_BIG = 95
+    EXTRA_BIG = 97
+    ANOMAL_BIG = 99
+    GREATEST_BIG = 100
+    BLACKSWAN_BIG = 100500
 
     def __lt__(self, other):  # operator <  # {{{
         assert isinstance(other, Size)
@@ -62,6 +79,7 @@ class Size(enum.Enum):
     def fromStr(cls, string_size: str):
         sizes = {
             "BLACKSWAN_SMALL": Size.BLACKSWAN_SMALL,
+            "GREATEST_SMALL": Size.GREATEST_SMALL,
             "ANOMAL_SMALL": Size.ANOMAL_SMALL,
             "EXTRA_SMALL": Size.EXTRA_SMALL,
             "VERY_SMALL": Size.VERY_SMALL,
@@ -75,6 +93,7 @@ class Size(enum.Enum):
             "VERY_BIG": Size.VERY_BIG,
             "EXTRA_BIG": Size.EXTRA_BIG,
             "ANOMAL_BIG": Size.ANOMAL_BIG,
+            "GREATEST_BIG": Size.GREATEST_BIG,
             "BLACKSWAN_BIG": Size.BLACKSWAN_BIG,
         }
         return sizes[string_size]
@@ -83,11 +102,13 @@ class Size(enum.Enum):
 
 
 class SimpleSize(enum.Enum):
-    XS = -3
-    S = -1
-    M = 0
-    L = 1
-    XL = 3
+    XXS = 0
+    XS = 20
+    S = 40
+    M = 60
+    L = 80
+    XL = 100
+    XXL = 100500
 
     def __eq__(self, other):  # operator ==  # {{{
         if isinstance(other, SimpleSize):
@@ -95,9 +116,11 @@ class SimpleSize(enum.Enum):
 
         assert isinstance(other, Size)
         match self:
+            case SimpleSize.XXS:
+                eq = (Size.BLACKSWAN_SMALL.value,)
             case SimpleSize.XS:
                 eq = (
-                    Size.BLACKSWAN_SMALL.value,
+                    Size.GREATEST_SMALL.value,
                     Size.ANOMAL_SMALL.value,
                     Size.EXTRA_SMALL.value,
                     Size.VERY_SMALL.value,
@@ -121,8 +144,10 @@ class SimpleSize(enum.Enum):
                     Size.VERY_BIG.value,
                     Size.EXTRA_BIG.value,
                     Size.ANOMAL_BIG.value,
-                    Size.BLACKSWAN_BIG.value,
+                    Size.GREATEST_BIG.value,
                 )
+            case SimpleSize.XXL:
+                eq = (Size.BLACKSWAN_BIG.value,)
 
         return other.value in eq
 
